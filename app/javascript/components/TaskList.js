5335d982084420e9f71dca9aed238a7ae145a13f
amelie <amelieoller@gmail.com>
2017-12-28 22:07:18 -0800

    Refactor task list

--- app/javascript/components/TaskList.js
+++ app/javascript/components/TaskList.js
@@ -7,21 +7,25 @@ const TaskList = ({ tasks, deleteTask }) => {
 
   return (
     <div className="row">
-      <div className="col-6">
-        <ul className="list-group">
-          {tasks.map(task => (
-            <li className="list-group-item" key={task.id}>
-              <span>{task.name}</span>{" "}
+      <h3>Your Tasks:</h3>
+      {tasks.map(task => (
+        <div className="col-12">
+          <div class="input-group mb-3">
+            <input type="text" value={task.name} />
+            <div class="input-group-append">
+              <button class="btn btn-outline-secondary" type="button">
+                Edit
+              </button>
               <button
                 className="btn btn-danger"
                 onClick={() => handleOnClick(task)}
               >
                 Delete
               </button>
-            </li>
-          ))}
-        </ul>
-      </div>
+            </div>
+          </div>
+        </div>
+      ))}
     </div>
   );
 };

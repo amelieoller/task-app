ff294cce80a6e5e1fde1319c2492206bfb4b47e3
amelie <amelieoller@gmail.com>
2017-12-29 07:38:31 -0800

    Modify create task to include id and create update task reducer

--- app/javascript/reducers/tasksReducer.js
+++ app/javascript/reducers/tasksReducer.js
@@ -5,16 +5,25 @@ export default function tasksReducer(state = initialState, action) {
   switch (action.type) {
     case types.LOAD_TASKS_SUCCESS:
       return action.tasks;
-
     case types.CREATE_TASK_SUCCESS:
+      // return [...state, action.crop]
       return [
-        ...state.filter(task => task.id !== action.task.id),
-        Object.assign({}, action.task)
+        ...state,
+        {
+          id: action.task.id,
+          completed: false,
+          name: action.task.name
+        }
       ];
-
+    case types.UPDATE_TASK_SUCCESS:
+      return state.map(
+        task =>
+          task.id === action.task.id
+            ? Object.assign({}, ...task, action.task)
+            : task
+      );
     case types.DELETE_TASK_SUCCESS:
-      return state.filter(task => task.id !== action.task.id);
-
+      return state.filter(task => task.id !== action.id);
     default:
       return state;
   }

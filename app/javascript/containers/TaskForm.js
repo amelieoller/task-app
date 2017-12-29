c8160570be4e04d8b0c67d9aede8a9119d2bb2aa
amelie <amelieoller@gmail.com>
2017-12-28 21:31:52 -0800

    Add Task form

---
+++ app/javascript/containers/TaskForm.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+
+class TaskForm extends Component {
+  state = {
+    name: ""
+  };
+
+  handleOnChange = e => {
+    const { name, value } = e.target;
+    this.setState({
+      [name]: value
+    });
+  };
+
+  handleOnSubmit = e => {
+    e.preventDefault();
+    this.props.addTask({ ...this.state });
+  };
+
+  render() {
+    return (
+      <div className="row">
+        <div className="col-6">
+          <form className="form-inline" onSubmit={e => this.handleOnSubmit(e)}>
+            <div className="form-group mx-sm-2 mb-2">
+              <input
+                placeholder="Create New Task"
+                className="form-control"
+                type="text"
+                name="name"
+                onChange={e => this.handleOnChange(e)}
+              />
+            </div>
+            <input type="submit" className="btn btn-primary mb-2" />
+          </form>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default TaskForm;

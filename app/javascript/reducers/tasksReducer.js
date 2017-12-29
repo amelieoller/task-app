967d808c5c437ec27343adb8e714c6d9d155f7b0
amelie <amelieoller@gmail.com>
2017-12-28 21:31:24 -0800

    Add load, create and delete reducers

--- app/javascript/reducers/tasksReducer.js
+++ app/javascript/reducers/tasksReducer.js
@@ -1,5 +1,20 @@
-export default function tasksReducer(state = [], action) {
+import * as types from "../actions/actionTypes";
+import initialState from "./initalState";
+
+export default function tasksReducer(state = initialState, action) {
   switch (action.type) {
+    case types.LOAD_TASKS_SUCCESS:
+      return action.tasks;
+
+    case types.CREATE_TASK_SUCCESS:
+      return [
+        ...state.filter(task => task.id !== action.task.id),
+        Object.assign({}, action.task)
+      ];
+
+    case types.DELETE_TASK_SUCCESS:
+      return state.filter(task => task.id !== action.task.id);
+
     default:
       return state;
   }

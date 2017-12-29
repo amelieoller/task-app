037d637fa2d7238b6e85fbd310022eaef2b6ea48
amelie <amelieoller@gmail.com>
2017-12-28 21:37:44 -0800

    Modify navbar to use bootstrap css

--- app/javascript/components/NavBar.js
+++ app/javascript/components/NavBar.js
@@ -2,14 +2,24 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 const NavBar = props => (
-  <nav className="navbar navbar-default">
-    <div className="container-fluid">
-      <ul className="nav navbar-nav">
-        <li>
-          <NavLink to="/">Home</NavLink>
+  <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <NavLink to="/" className="navbar-brand">
+      My Tasks
+    </NavLink>
+    <button className="navbar-toggler" type="button" data-toggle="collapse">
+      <span className="navbar-toggler-icon" />
+    </button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav mr-auto">
+        <li className="nav-item active">
+          <NavLink to="/" className="nav-link">
+            Home
+          </NavLink>
         </li>
-        <li>
-          <NavLink to="/tasks">Tasks</NavLink>
+        <li className="nav-item">
+          <NavLink to="/tasks" className="nav-link">
+            Tasks
+          </NavLink>
         </li>
       </ul>
     </div>

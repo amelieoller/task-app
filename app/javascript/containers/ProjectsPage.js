e127561df2727a67f567628a13242459711dc3ff
amelie <amelieoller@gmail.com>
2017-12-30 18:06:15 -0800

    Add project page

---
+++ app/javascript/containers/ProjectsPage.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import * as ProjectActions from "../actions/projectActions";
+import ProjectList from "../components/ProjectList";
+
+import ProjectForm from "../containers/ProjectForm";
+import { bindActionCreators } from "redux";
+
+class ProjectsPage extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Projects</h2>
+        <ProjectForm createProject={this.props.actions.createProject} />
+        <ProjectList
+          projects={this.props.projects}
+          deleteProject={this.props.actions.deleteProject}
+          updateProject={this.props.actions.updateProject}
+          checkProject={this.props.actions.checkProject}
+        />
+      </div>
+    );
+  }
+}
+
+function mapStateToProps(state, ownProps) {
+  return {
+    projects: state.projects
+  };
+}
+
+const mapDispatchToProps = dispatch => ({
+  actions: bindActionCreators(ProjectActions, dispatch)
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(ProjectsPage);

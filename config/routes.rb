739eeb3133abdc7ae69ccc31fef86dc7844ab338
amelie <amelieoller@gmail.com>
2018-01-11 17:02:26 -0800

    Fix rails routing problem

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  root to: 'home#index'  
   namespace :api do
     resources :tasks, except: [:new, :edit]
     resources :projects, except: [:new, :edit]
   end
+  root to: 'home#index'
+  get '*path', to: 'home#index'
 end
+

d4e4442638b1e7b7916d267cc263de22076a077c
amelie <amelieoller@gmail.com>
2018-01-21 19:00:17 -0800

    Edit project suggest to use material-ui css

--- app/javascript/containers/ProjectSuggest.js
+++ app/javascript/containers/ProjectSuggest.js
@@ -1,115 +1,200 @@
-import React from "react";
-import Autosuggest from "react-autosuggest";
-import { connect } from "react-redux";
-import { bindActionCreators } from "redux";
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import React from 'react';
+import PropTypes from 'prop-types';
+import Autosuggest from 'react-autosuggest';
+import match from 'autosuggest-highlight/match';
+import parse from 'autosuggest-highlight/parse';
+import TextField from 'material-ui/TextField';
+import Paper from 'material-ui/Paper';
+import { MenuItem } from 'material-ui/Menu';
+import { withStyles } from 'material-ui/styles';
+import compose from 'recompose/compose';
 
-import * as ProjectActions from "../actions/projectActions";
+import * as ProjectActions from '../actions/projectActions';
+
+function renderInput(inputProps) {
+	const { classes, autoFocus, value, ref, ...other } = inputProps;
+
+	return (
+		<TextField
+			className={classes.textField}
+			margin="normal"
+			value={value}
+			label="Project"
+			inputRef={ref}
+			InputProps={{
+				classes: {
+					input: classes.input
+				},
+				onBlur,
+				...other
+			}}
+		/>
+	);
+}
+
+function renderSuggestion(suggestion, { query, isHighlighted }) {
+	const matches = match(suggestion.name, query);
+	const parts = parse(suggestion.name, matches);
+
+	return (
+		<MenuItem selected={isHighlighted} component="div">
+			<div>
+				{parts.map((part, index) => {
+					return part.highlight ? (
+						<span key={String(index)} style={{ fontWeight: 300 }}>
+							{part.text}
+						</span>
+					) : (
+						<strong key={String(index)} style={{ fontWeight: 500 }}>
+							{part.text}
+						</strong>
+					);
+				})}
+			</div>
+		</MenuItem>
+	);
+}
+
+function renderSuggestionsContainer(options) {
+	const { containerProps, children } = options;
+
+	return (
+		<Paper {...containerProps} square>
+			{children}
+		</Paper>
+	);
+}
+
+function getSuggestionValue(suggestion) {
+	// if (suggestion.isAddNew) {
+	//   this.props.actions.createProject({ name: this.state.value });
+	//   this.props.handleProjectCreation(suggestion)
+	//   return this.state.value;
+	// }
+	// this.props.handleProjectSuggest(suggestion.id)
+	return suggestion.name;
+}
+
+function onBlur(event, { highlightedSuggestion }) {}
+
+const styles = theme => ({
+	container: {
+		// flexGrow: 1,
+		position: 'relative'
+		// height: 200,
+	},
+	suggestionsContainerOpen: {
+		position: 'absolute',
+		marginTop: theme.spacing.unit,
+		marginBottom: theme.spacing.unit * 3,
+		left: 0,
+    right: 0,
+    'z-index': 1
+	},
+	suggestion: {
+		display: 'block'
+	},
+	suggestionsList: {
+		margin: 0,
+		padding: 0,
+		listStyleType: 'none'
+	},
+	textField: {
+		marginLeft: theme.spacing.unit,
+		marginRight: theme.spacing.unit,
+		width: '100%'
+	}
+});
 
 class ProjectSuggest extends React.Component {
-  constructor() {
-    super();
+	state = {
+		value: '',
+		suggestions: []
+	};
 
-    this.state = {
-      value: "",
-      suggestions: []
-    };
-  }
+	getSuggestions = value => {
+		const inputValue = value.trim().toLowerCase();
+		const inputLength = inputValue.length;
+		let count = 0;
+		return inputLength === 0
+			? []
+			: // ? [{ isAddNew: true }]
+				this.props.projects.filter(suggestion => {
+					const keep =
+						count < 5 &&
+						suggestion.name.toLowerCase().slice(0, inputLength) === inputValue;
 
-  escapeRegexCharacters = str => str.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
+					if (keep) {
+						count += 1;
+					}
 
-  getSuggestions = value => {
-    const escapedValue = this.escapeRegexCharacters(value.trim());
+					return keep;
+				});
+	};
 
-    if (escapedValue === "") {
-      return [];
-    }
+	handleSuggestionsFetchRequested = ({ value }) => {
+		this.setState({
+			suggestions: this.getSuggestions(value)
+		});
+	};
 
-    const regex = new RegExp("^" + escapedValue, "i");
-    const suggestions = this.props.projects.filter(language =>
-      regex.test(language.name)
-    );
+	handleSuggestionsClearRequested = () => {
+		this.setState({
+			suggestions: []
+		});
+	};
 
-    if (suggestions.length === 0) {
-      return [{ isAddNew: true }];
-    }
+	handleChange = (event, { newValue }) => {
+		this.setState({
+			value: newValue
+		});
+	};
 
-    return suggestions;
-  };
+	handleBlur = (event, { highlightedSuggestion }) => {
+		console.log(`Project name: ${this.state.value}`);
+	};
 
-  onChange = (event, { newValue, method }) => {
-    this.setState({
-      value: newValue
-    });
-  };
+	render() {
+		const { classes } = this.props;
 
-  getSuggestionValue = suggestion => {
-    if (suggestion.isAddNew) {
-      this.props.actions.createProject({ name: this.state.value });
-      this.props.handleProjectCreation(suggestion)
-      return this.state.value;
-    }
-    this.props.handleProjectSuggest(suggestion.id)
-    return suggestion.name;
-  };
-
-  renderSuggestion = suggestion => {
-    if (suggestion.isAddNew) {
-      return (
-        <span>
-          [+] Add new: <strong>{this.state.value}</strong>
-        </span>
-      );
-    }
-
-    return suggestion.name;
-  };
-
-  onSuggestionsFetchRequested = ({ value }) => {
-    this.setState({
-      suggestions: this.getSuggestions(value)
-    });
-  };
-
-  onSuggestionsClearRequested = () => {
-    this.setState({
-      suggestions: []
-    });
-  };
-
-  render() {
-    const { value, suggestions } = this.state;
-    const inputProps = {
-      name: "project",
-      title: "Project",
-      placeholder: "Project",
-      value,
-      onChange: this.onChange
-    };
-    const theme = {
-      container: "autosuggest",
-      input: "form-control",
-      suggestionsContainer: "dropdown",
-      suggestionsList: `dropdown-menu ${suggestions.length ? "show" : ""}`,
-      suggestion: "dropdown-item",
-      suggestionFocused: "active"
-    };
-
-    return (
-      <Autosuggest
-        suggestions={suggestions}
-        onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
-        onSuggestionsClearRequested={this.onSuggestionsClearRequested}
-        getSuggestionValue={this.getSuggestionValue}
-        renderSuggestion={this.renderSuggestion}
-        inputProps={inputProps}
-        theme={theme}
-      />
-    );
-  }
+		return (
+			<Autosuggest
+				theme={{
+					container: classes.container,
+					suggestionsContainerOpen: classes.suggestionsContainerOpen,
+					suggestionsList: classes.suggestionsList,
+					suggestion: classes.suggestion
+				}}
+				renderInputComponent={renderInput}
+				suggestions={this.state.suggestions}
+				onSuggestionsFetchRequested={this.handleSuggestionsFetchRequested}
+				onSuggestionsClearRequested={this.handleSuggestionsClearRequested}
+				renderSuggestionsContainer={renderSuggestionsContainer}
+				getSuggestionValue={getSuggestionValue}
+				renderSuggestion={renderSuggestion}
+				onSuggestionSelected={this.clearInput}
+				inputProps={{
+					classes,
+					placeholder: 'Enter a Project',
+					onBlur: this.handleBlur,
+					value: this.state.value,
+					onChange: this.handleChange
+				}}
+			/>
+		);
+	}
 }
 
 const mapDispatchToProps = dispatch => ({
-  actions: bindActionCreators(ProjectActions, dispatch)
+	actions: bindActionCreators(ProjectActions, dispatch)
 });
 
-export default connect(null, mapDispatchToProps)(ProjectSuggest);
+ProjectSuggest.propTypes = {
+	classes: PropTypes.object.isRequired
+};
+
+export default compose(withStyles(styles), connect(null, mapDispatchToProps))(
+	ProjectSuggest
+);

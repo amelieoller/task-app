dd85a2ca8522830ef5973c1b80d58c378e91ef64
amelie <amelieoller@gmail.com>
2018-01-21 18:59:51 -0800

    Remove unused code in drawer

--- app/javascript/components/ToggleDrawer.js
+++ app/javascript/components/ToggleDrawer.js
@@ -6,10 +6,6 @@ import Drawer from 'material-ui/Drawer';
 import Button from 'material-ui/Button';
 import Divider from 'material-ui/Divider';
 import List, { ListItem, ListItemIcon, ListItemText } from 'material-ui/List';
-import InboxIcon from 'material-ui-icons/Inbox';
-import DraftsIcon from 'material-ui-icons/Drafts';
-import IconButton from 'material-ui/IconButton';
-import MenuIcon from 'material-ui-icons/Menu';
 import Icon from 'material-ui/Icon';
 
 const styles = {

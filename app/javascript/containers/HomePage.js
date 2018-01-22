3de5bef4fd567d3917f6da5f4e7cea81be47acb4
amelie <amelieoller@gmail.com>
2018-01-21 18:59:26 -0800

    Edit home and tasks page to use material-ui css

--- app/javascript/containers/HomePage.js
+++ app/javascript/containers/HomePage.js
@@ -1,32 +1,25 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import * as TaskActions from "../actions/taskActions";
-import { bindActionCreators } from "redux";
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as TaskActions from '../actions/taskActions';
+import { bindActionCreators } from 'redux';
 
-import ProjectsPage from "./ProjectsPage";
-import TasksPage from "./TasksPage";
+import ProjectsPage from './ProjectsPage';
+import TasksPage from './TasksPage';
 
 class HomePage extends Component {
-  render() {
-    return (
-      <div className="row">
-        <div className="col">
-          <h1>Homepage</h1>
-          <TasksPage />
-        </div>
-      </div>
-    );
-  }
+	render() {
+		return <TasksPage />;
+	}
 }
 
 function mapStateToProps(state, ownProps) {
-  return {
-    tasks: state.tasks
-  };
+	return {
+		tasks: state.tasks
+	};
 }
 
 const mapDispatchToProps = dispatch => ({
-  actions: bindActionCreators(TaskActions, dispatch)
+	actions: bindActionCreators(TaskActions, dispatch)
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(HomePage);
--- app/javascript/containers/TasksPage.js
+++ app/javascript/containers/TasksPage.js
@@ -1,38 +1,39 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import { bindActionCreators } from "redux";
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import Typography from 'material-ui/Typography';
 
-import * as TaskActions from "../actions/taskActions";
-import TaskList from "../components/TaskList";
-import TaskForm from "../containers/TaskForm";
+import * as TaskActions from '../actions/taskActions';
+import TaskList from '../components/TaskList';
+import TaskForm from '../containers/TaskForm';
 
 class TasksPage extends Component {
-  render() {
-    return (
-      <div className="row">
-        <div className="col">
-          <h2>Tasks</h2>
-          <TaskForm createTask={this.props.actions.createTask} />
-          <TaskList
-            tasks={this.props.tasks}
-            deleteTask={this.props.actions.deleteTask}
-            updateTask={this.props.actions.updateTask}
-            checkTask={this.props.actions.checkTask}
-          />
-        </div>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div>
+				<Typography type="display2" gutterBottom>
+					Tasks{' '}
+				</Typography>
+				<TaskForm createTask={this.props.actions.createTask} />
+				<TaskList
+					tasks={this.props.tasks}
+					deleteTask={this.props.actions.deleteTask}
+					updateTask={this.props.actions.updateTask}
+					checkTask={this.props.actions.checkTask}
+				/>
+			</div>
+		);
+	}
 }
 
 function mapStateToProps(state, ownProps) {
-  return {
-    tasks: state.tasks
-  };
+	return {
+		tasks: state.tasks
+	};
 }
 
 const mapDispatchToProps = dispatch => ({
-  actions: bindActionCreators(TaskActions, dispatch)
+	actions: bindActionCreators(TaskActions, dispatch)
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(TasksPage);

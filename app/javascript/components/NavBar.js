586ed729ea53dc4d0fdd796d97524f89195baa78
amelie <amelieoller@gmail.com>
2018-01-21 19:51:32 -0800

    Edit top margin

--- app/javascript/components/NavBar.js
+++ app/javascript/components/NavBar.js
@@ -18,8 +18,7 @@ const drawerWidth = 240;
 const styles = theme => ({
 	root: {
 		width: '100%',
-		// height: 430,
-		marginTop: theme.spacing.unit * 3,
+		marginTop: 0,
 		zIndex: 1,
 		overflow: 'hidden'
 	},

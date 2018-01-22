ab3b533fd5fec6d8a7b86477473ca5af50abd77d
amelie <amelieoller@gmail.com>
2018-01-21 19:01:31 -0800

    Edit task form to use material-ui css

--- app/javascript/containers/TaskForm.js
+++ app/javascript/containers/TaskForm.js
@@ -1,126 +1,174 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import ProjectSuggest from "./ProjectSuggest";
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import ProjectSuggest from './ProjectSuggest';
+import { withStyles } from 'material-ui/styles';
+import MenuItem from 'material-ui/Menu/MenuItem';
+import TextField from 'material-ui/TextField';
+import Button from 'material-ui/Button';
+import Done from 'material-ui-icons/Done';
+import Grid from 'material-ui/Grid';
+import {
+	FormControl,
+	FormHelperText,
+	FormGroup,
+	FormControlLabel
+} from 'material-ui/Form';
+import Select from 'material-ui/Select';
+
+import compose from 'recompose/compose';
+
+const styles = theme => ({
+	container: {
+		display: 'flex',
+		flexWrap: 'wrap'
+	},
+	textField: {
+		marginLeft: theme.spacing.unit,
+		marginRight: theme.spacing.unit,
+		width: '100%'
+	},
+	button: {
+		margin: theme.spacing.unit,
+		'margin-bottom': 0
+	},
+	root: {
+		flexGrow: 1
+	}
+});
 
 class TaskForm extends Component {
-  state = {
-    name: "",
-    project_id: "",
-    time: 25,
-    priority: 4
-  };
+	state = {
+		name: '',
+		project_id: '',
+		time: '',
+		priority: ''
+	};
 
-  handleOnChange = e => {
-    const { name, value } = e.target;
-    this.setState({
-      [name]: value
-    });
-  };
+	handleOnChange = e => {
+		const { name, value } = e.target;
+		this.setState({
+			[name]: value
+		});
+	};
 
-  handleProjectSuggest = id => {
-    this.setState({
-      project_id: id
-    });
-  };
+	handleProjectSuggest = id => {
+		this.setState({
+			project_id: id
+		});
+	};
 
-  handleProjectCreation = newProject => {
-    console.log(newProject);
-  };
+	handleProjectCreation = newProject => {
+		console.log(newProject);
+	};
 
-  handleOnSubmit = e => {
-    if (this.state.name) {
-      if (e.which === 13 || e.target.type === "button") {
-        this.props.createTask({ ...this.state });
-        this.setState({ name: "", project_id: "", time: 25, priority: 4 });
-      }
-    }
-  };
+	handleOnSubmit = e => {
+		if (this.state.name) {
+			if (e.which === 13 || e.currentTarget.type === 'button') {
+				this.props.createTask({ ...this.state });
+				this.setState({ name: '', project_id: '', time: '', priority: '' });
+			}
+		}
+	};
 
-  render() {
-    return (
-      <form className="form-row align-items-center">
-        {/* Task Name Input */}
-        <div className="col">
-          <label className="sr-only" htmlFor="taskFormInput">
-            New Task
-          </label>
-          <input
-            type="text"
-            className="form-control mb-2"
-            id="taskFormInput"
-            placeholder="Create New Task"
-            name="name"
-            title="Task Name"
-            value={this.state.name}
-            onChange={this.handleOnChange}
-            onKeyDown={this.handleOnSubmit}
-          />
-        </div>
+	render() {
+		const { classes, autoFocus } = this.props;
 
-        {/* Time Input */}
-        <div className="col-lg-1 col-2">
-          <label className="sr-only" htmlFor="timeFormInput">
-            Minutes
-          </label>
-          <input
-            type="number"
-            className="form-control mb-2"
-            id="timeFormInput"
-            placeholder="Minutes"
-            name="time"
-            title="Minutes"
-            value={this.state.time}
-            onChange={this.handleOnChange}
-            onKeyDown={this.handleOnSubmit}
-          />
-        </div>
+		return (
+			<div className={classes.root}>
+				<form className={classes.container} noValidate autoComplete="off">
+					<Grid container spacing={8} alignItems="center">
+						{/* Task Name Input */}
+						<Grid item xs>
+							<TextField
+								autoFocus={true}
+								id="taskFormInput"
+								label="New Task"
+								placeholder="Enter a Task Name"
+								className={classes.textField}
+								margin="normal"
+								name="name"
+								value={this.state.name}
+								onChange={this.handleOnChange}
+								onKeyDown={this.handleOnSubmit}
+							/>
+						</Grid>
 
-        {/* Project Select */}
-        <div className="col-auto">
-          <div className="mb-2">
-            <ProjectSuggest
-              projects={this.props.projects}
-              handleProjectSuggest={this.handleProjectSuggest}
-              handleProjectCreation={this.handleProjectCreation}
-            />
-          </div>
-        </div>
+						{/* Time Input */}
+						<Grid item xs={2}>
+							<TextField
+								id="timeFormInput"
+								type="number"
+								label="Minutes"
+								placeholder="Time"
+								className={classes.textField}
+								margin="normal"
+								name="time"
+								title="Minutes"
+								value={this.state.time}
+								onChange={this.handleOnChange}
+								onKeyDown={this.handleOnSubmit}
+							/>
+						</Grid>
 
-        {/* Priority Select */}
-        <div className="col-auto">
-          <select
-            className="custom-select mb-2"
-            name="priority"
-            title="Priority"
-            value={this.state.priority}
-            onChange={this.handleOnChange}
-          >
-            <option />
-            <option value="1">1</option>
-            <option value="2">2</option>
-            <option value="3">3</option>
-          </select>
-        </div>
+						{/* Project Select */}
+						<Grid item xs={2}>
+							<ProjectSuggest
+								projects={this.props.projects}
+								handleProjectSuggest={this.handleProjectSuggest}
+								handleProjectCreation={this.handleProjectCreation}
+							/>
+						</Grid>
 
-        <div className="col-auto">
-          <button
-            type="button"
-            className="btn btn-default mb-2"
-            onClick={this.handleOnSubmit}
-          >
-            Submit
-          </button>
-        </div>
-      </form>
-    );
-  }
+						{/* Priority Select */}
+						<Grid item xs={2}>
+							<TextField
+								id="prioritySelect"
+								select
+								label="Priority"
+								className={classes.textField}
+								name="priority"
+								value={this.state.priority}
+								onChange={this.handleOnChange}
+								SelectProps={{
+									MenuProps: {
+										className: classes.menu
+									}
+								}}
+								margin="normal"
+							>
+								{[1, 2, 3].map(option => (
+									<MenuItem key={option} value={option}>
+										{option}
+									</MenuItem>
+								))}
+							</TextField>
+						</Grid>
+
+						{/* Submit button */}
+						<Grid item>
+							<Button
+								className={classes.button}
+								raised
+								color="contrast"
+								onClick={this.handleOnSubmit}
+							>
+								<Done className={classes.leftIcon} />
+								Done
+							</Button>
+						</Grid>
+
+					</Grid>
+				</form>
+			</div>
+		);
+	}
 }
 
 function mapStateToProps(state, ownProps) {
-  return {
-    tasks: state.tasks,
-    projects: state.projects
-  };
+	return {
+		tasks: state.tasks,
+		projects: state.projects
+	};
 }
 
-export default connect(mapStateToProps)(TaskForm);
+export default compose(withStyles(styles), connect(mapStateToProps))(TaskForm);

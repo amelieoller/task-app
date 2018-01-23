f03356477d35cb8530ba191072818388d76e7c83
amelie <amelieoller@gmail.com>
2018-01-23 11:11:25 -0800

    Add project name to initial state

--- app/javascript/reducers/taskInitalState.js
+++ app/javascript/reducers/taskInitalState.js
@@ -1,12 +1,13 @@
 export default [
-  {
-    name: "",
-    completed: false,
-    id: 0,
-    priority: 0,
-    due_date: null,
-    project_id: null,
-    time: 0,
-    position: 0
-  }
+	{
+		name: '',
+		completed: false,
+		id: 0,
+		priority: 0,
+		due_date: null,
+		project_id: null,
+		time: 0,
+		position: 0,
+		project_name: ''
+	}
 ];
